bbd44313070ab2472054b973bdfce25491fa7aff
Yuliya Zhoglik <YuliyaZhoglik@users.noreply.github.com>
2020-01-14 02:13:31 +0700

    Add UI

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] private SettingsPopup PanelMain;
+    [SerializeField] private SettingsPopup PanelColor;
+    void Start()
+    {
+        OnCloseSettings();
+    }
+
+    public void OnOpenSettings()
+    {
+        PanelMain.Close();
+        PanelColor.Open();
+    }
+
+    public void OnCloseSettings()
+    {
+        PanelMain.Open();
+        PanelColor.Close();
+    }
+}
